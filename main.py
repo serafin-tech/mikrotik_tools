4456f9a42bad22d0cfd02c2dbec9d4f97ecf5d1b
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2023-02-05 17:26:53 +0100

    subparser for options + option 121 for classless routes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import argparse
 import ipaddress
 import logging
+import pprint
+from typing import List
 
 
 def encode_unifi_43(payload: str) -> str:
@@ -18,35 +20,87 @@ def encode_unifi_43(payload: str) -> str:
     return f'{prefix}{ip_addr_hex}'
 
 
+def encode_classless_routes(payload: List[str]) -> str:
+    prefix = '0x0'  # prefix to use for the option
+    ret = []
+
+    for route_spec in payload:
+        logging.debug("processing %s...", route_spec)
+        network, gateway = route_spec.split(' ')
+        logging.debug("network: %s, gateway: %s", network, gateway)
+
+        try:
+            network_addr = ipaddress.ip_network(network)
+            gateway_addr = ipaddress.ip_address(gateway)
+
+            if network_addr.prefixlen > 24:
+                network_addr_hex_length = 8
+            elif 16 < network_addr.prefixlen <= 24:
+                network_addr_hex_length = 6
+            elif 8 < network_addr.prefixlen <= 16:
+                network_addr_hex_length = 4
+            elif 0 < network_addr.prefixlen <= 8:
+                network_addr_hex_length = 2
+            else:
+                network_addr_hex_length = 0
+
+            network_addr_hex = f'{network_addr.network_address:X}'[:network_addr_hex_length]
+
+            logging.debug("network_address: %s, prefixlen: %s, network_addr_hex: %s",
+                          network_addr.network_address, network_addr.prefixlen, network_addr_hex)
+
+            ret.append(f'{network_addr.prefixlen:X}{network_addr_hex}{gateway_addr:X}')
+
+        except ValueError:
+            logging.info('invalid route spec: %s', route_spec)
+            raise
+
+    return f"{prefix}{''.join(ret)}"
+
+
 def args_parser():
     parser = argparse.ArgumentParser(description='Script to generate MikroTik DHCP options')
 
-    parser.add_argument('option',
-                        choices=['unifi_43'],
-                        help='option to generate')
-    parser.add_argument('payload',
-                        nargs='?',
-                        help='payload to encode')
     parser.add_argument('-v', '--verbose',
                         action='store_true',
                         default=False,
                         help='talkative mode')
 
+    subparsers = parser.add_subparsers(dest='option')
+
+    unifi_43 = subparsers.add_parser('unifi_43', help='Option 43 for Unifi Controller address')
+    unifi_43.add_argument('address',
+                          help='address to encode')
+
+    classless_routes = subparsers.add_parser('classless_routes', help='Option 121 for classless static routes')
+    classless_routes.add_argument('route_spec',
+                                  nargs='+',
+                                  help='route specification to encode in format "a.b.c.d/e 1.2.3.4", '
+                                       'first spec MUST be for default route in format "0.0.0.0/0 1.2.3.4"')
+
     params = parser.parse_args()
 
-    if params.verbose:
+    return params
+
+
+def logging_setup(verbose: bool):
+    if verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    return params.option, params.payload
-
 
 def main():
-    option, payload = args_parser()
+    params = args_parser()
 
-    if option == 'unifi_43':
-        print(encode_unifi_43(payload))
+    logging_setup(params.verbose)
+
+    if params.option == 'unifi_43':
+        logging.debug(pprint.pformat(params))
+        logging.info("encoded value: %s", encode_unifi_43(params.address))
+    elif params.option == 'classless_routes':
+        logging.debug(pprint.pformat(params))
+        logging.info("encoded value: %s", encode_classless_routes(params.route_spec))
 
 
 if __name__ == '__main__':
